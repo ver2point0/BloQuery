3b85ffc836f16bc5aed126cdf5a461c740696767
John McIntosh II <jmci10@hotmail.com>
2015-06-15 21:42:17 -0400

    more create a bloquery account progress

--- app/src/main/java/com/ver2point0/android/blocquery/ui/activity/BloQueryActivity.java
+++ app/src/main/java/com/ver2point0/android/blocquery/ui/activity/BloQueryActivity.java
@@ -1,7 +1,14 @@
 package com.ver2point0.android.blocquery.ui.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.ver2point0.android.blocquery.R;
 
 public class BloQueryActivity extends Activity {
 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bloquery);
+    }
 }
