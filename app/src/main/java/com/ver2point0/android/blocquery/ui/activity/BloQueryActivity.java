9269089358cc1af32cf059e9e66258304eeb66e2
John McIntosh II <jmci10@hotmail.com>
2015-06-17 02:29:52 -0400

    more create a bloquery account progress

--- app/src/main/java/com/ver2point0/android/blocquery/ui/activity/BloQueryActivity.java
+++ app/src/main/java/com/ver2point0/android/blocquery/ui/activity/BloQueryActivity.java
@@ -1,15 +1,7 @@
 package com.ver2point0.android.blocquery.ui.activity;
 
 import android.app.Activity;
-import android.app.LoaderManager;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.AutoCompleteTextView;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.ver2point0.android.blocquery.R;
 
@@ -18,37 +10,37 @@ import com.ver2point0.android.blocquery.R;
 * Android login screen Activity
 * */
 
-public class BloQueryActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor>{
+public class BloQueryActivity extends Activity {
 
-
+//    implements LoaderManager.LoaderCallbacks<Cursor>
     private static final String DUMMY_CREDENTIALS = "user@test.com:hello";
 
-    private UserLoginTask mUserLoginTask = null;
-    private View mLoginFormView;
-    private View mProgressView;
-    private AutoCompleteTextView mEmailTextView;
-    private EditText mPasswordTextView;
-    private TextView mSignUpTextView;
+//    private UserLoginTask mUserLoginTask = null;
+//    private View mLoginFormView;
+//    private View mProgressView;
+//    private AutoCompleteTextView mEmailTextView;
+//    private EditText mPasswordTextView;
+//    private TextView mSignUpTextView;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bloquery);
 
-        mEmailTextView = (AutoCompleteTextView) findViewById(R.id.actv_email);
-        loadAutoComplete();
-
-        mPasswordTextView = (EditText) findViewById(R.id.et_password);
-        mPasswordTextView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == EditorInfo.IME_NULL) {
-                    initLogin();
-                    return true;
-                }
-                return false;
-            }
-        });
+//        mEmailTextView = (AutoCompleteTextView) findViewById(R.id.actv_email);
+//        loadAutoComplete();
+//
+//        mPasswordTextView = (EditText) findViewById(R.id.et_password);
+//        mPasswordTextView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+//                if (id == EditorInfo.IME_NULL) {
+//                    initLogin();
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
 
 
 
