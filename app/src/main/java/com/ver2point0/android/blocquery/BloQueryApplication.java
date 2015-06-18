115380482d977305778555a6408d3e64d0b17053
John McIntosh II <jmci10@hotmail.com>
2015-06-17 21:37:37 -0400

    Connected BloQuery to Parse

--- app/src/main/java/com/ver2point0/android/blocquery/BloQueryApplication.java
+++ app/src/main/java/com/ver2point0/android/blocquery/BloQueryApplication.java
@@ -2,12 +2,14 @@ package com.ver2point0.android.blocquery;
 
 import android.app.Application;
 
+import com.parse.Parse;
+
 public class BloQueryApplication extends Application {
 
-    /*
-    * Add one time initializations in this application class like Parse user API
-    * */
     @Override
     public void onCreate() {
+        // Enable Parse Local DataStore
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "KbF3oiduq4h9bsIBfycOW5pS5pXWfFyJvdOaTx71", "VEWWrRR4pk35VY1eXFYr0WPuky4kQ8RziMIlI8QS");
     }
 }
